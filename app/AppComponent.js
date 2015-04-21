f14cb77deaf55e00f28eda0e5544154f1234bf00
Daniel Reszka <dannn.r@gmail.com>
2015-04-21 19:43:24 +0200

    Use react-bootstrap components for navigation and grid

--- app/AppComponent.js
+++ app/AppComponent.js
@@ -1,14 +1,26 @@
 var React = require('react');
-var NavComponent = require('./NavComponent.js');
-var GroupsComponent = require('./GroupsComponent.js');
+
+var Grid = require('react-bootstrap').Grid;
+var Row  = require('react-bootstrap').Row;
+var Col  = require('react-bootstrap').Col;
+
+var Nav = require('./NavComponent.js');
+
 
 module.exports = React.createClass({
   render: function(){
     return(
-      <div>
-        <NavComponent />
-        <GroupsComponent />
-      </div>
+      <Grid>
+        <Row><Nav/></Row>
+        <Row className="show-grid">
+          <Col xs={12} md={8}>
+            Main content
+          </Col>
+          <Col xs={6} md={4}>
+            Secondary content
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 });
