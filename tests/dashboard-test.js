fb51d2c42b38ac16a1d486c429494252db8e8a3e
Daniel Reszka <dannn.r@gmail.com>
2015-04-30 21:28:16 +0200

    Implement basic test suite using Mocha
    * Run with: `npm test` or `npm run test`
    
    * Reasoning:
    	* A lot faster than Jest
    	* Better error messages
    	* Better display of test results
    
    * Based on article:
      http://www.hammerlab.org/2015/02/14/testing-react-web-apps-with-mocha/

---
+++ tests/dashboard-test.js
@@ -0,0 +1,15 @@
+require('./support/testdom')('<html><body></body></html>');
+
+var assert    = require('assert');
+var React     = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+
+describe('Dashboard', function() {
+
+  it('Works !', function() {
+    var Dashboard = require('../app/components/dashboard.js');
+    var dashboard = TestUtils.renderIntoDocument( <Dashboard /> );
+    assert.equal(dashboard.getDOMNode().textContent, 'DASHBOARD');
+  });
+
+});
