9a5373200d22b8e365ecec42a26be9758cf75ecf
Daniel Reszka <dannn.r@gmail.com>
2015-05-21 16:03:33 +0200

    Improve group page
    * Adds group description (faked)
    * Adds discussion with comments (faked)
    	* Introduces new component "Comment"

--- app/components/groups/group.js
+++ app/components/groups/group.js
@@ -9,6 +9,9 @@ var GroupStore   = require('./group_store')
 var B  = require('react-bootstrap')
 var Link  = require('react-router').Link
 
+var faker = require("faker");
+
+
 
 var Container = React.createClass({
   mixins: [Reflux.connect(GroupStore, "item")],
@@ -35,22 +38,50 @@ var Container = React.createClass({
 var Group = React.createClass({
   render: function() {
     var group = this.props.group
+
+
     return(
       <div>
-        <B.PageHeader>{group.name} <small>description</small></B.PageHeader>
+        <B.PageHeader>{group.name}<br /><small>{group.description}</small></B.PageHeader>
+
         <ul className="list-inline">
           { group.members.map(function(m, i){
             return (
               <li>
                 <Link to="person" params={{personId: m.id}}>
-                  <img src={m.avatar} className="img-circle" width="80" alt={m.name + ' avatar'} title={m.name} />
+                  <img src={m.avatar} style={{marginBottom: '10px'}} className="img-circle" width="40" alt={m.name + ' avatar'} title={m.name} />
                 </Link>
               </li>
             )
           })}
         </ul>
-        <hr/>
-        <p>(Details)</p>
+
+
+        <br />
+        <br />
+
+
+        <h3>Discussions</h3>
+
+        { group.members.map(function(m, i){
+          return (
+            <div>
+              <Comment />
+              <Comment>
+                <Comment />
+                <Comment />
+                <Comment />
+              </Comment>
+              <Comment />
+              <Comment />
+            </div>
+          )
+        })}
+        <hr />
+
+
+
+
 
       </div>
     );
@@ -58,5 +89,29 @@ var Group = React.createClass({
 
 });
 
+var Comment = React.createClass({
+  render: function() {
+      var avatar = faker.internet.avatar();
+      var name   = faker.name.findName();
+    return(
+      <div>
+          <hr />
+          <B.Row>
+            <B.Col md={1}>
+              <img src={avatar} className="img-circle" width="40" alt={name + ' avatar'} title={name} />
+            </B.Col>
+            <B.Col md={10}>
+              { faker.lorem.sentences(3)}
+              <div>
+                <small className="text-muted">{2} days</small>
+              </div>
+              {this.props.children}
+            </B.Col>
+          </B.Row>
+      </div>
+    );
+  }
+
+});
 
 module.exports = Container;
