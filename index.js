3b37751febe6351b7df3405e6b5b722c16683cbe
Daniel Reszka <dannn.r@gmail.com>
2015-05-29 17:52:51 +0200

    Change express server port from 3100 to 5000
    * Attempt to make it work on heroku

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + "/build"));
-app.listen(3100, function() { console.log('listening')});
+app.listen(5000, function() { console.log('listening')});
