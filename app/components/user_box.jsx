78f7507a0d4f298f45df3c3aa7bb3095f9b32bac
Daniel Reszka <dannn.r@gmail.com>
2015-07-07 13:14:27 +0200

    Add basic login form inside UserBox component
    * Triggers action:
    	`SessionActions.login(username, password)`

--- app/components/user_box.jsx
+++ app/components/user_box.jsx
@@ -6,15 +6,12 @@ var Reflux = require('reflux');
 var SessionStore   = require('../stores/session_store');
 var SessionActions = require('../actions/session_actions');
 
+var B  = require('react-bootstrap')
+
 
 var UserBoxComponent = React.createClass({
 
   mixins: [Reflux.connect(SessionStore, "user")],
-
-
-  componentDidMount: function () {
-    SessionActions.login('example@example.com', 'password');
-  },
 
   render: function() {
     var user  = this.state.user;
@@ -22,11 +19,49 @@ var UserBoxComponent = React.createClass({
     if (user && user.email) {
       var user_display = "Logged in as: " + user.email;
     } else {
-      var user_display = <a href="login">log in</a>;
+      var user_display = <LoginFormComponent />;
     }
 
     return (
       <div>{ user_display }</div>
+    );
+  }
+
+});
+
+var LoginFormComponent = React.createClass({
+
+  handleSubmit: function(e){
+    e.preventDefault();
+    SessionActions.login(
+      this.refs.username.getValue(),
+      this.refs.password.getValue()
+    );
+  },
+
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit} className='form-horizontal'>
+      <B.Input
+        type='text'
+        defaultValue='example@example.com'
+        label='Email'
+        ref='username'
+        labelClassName='col-xs-2'
+        wrapperClassName='col-xs-4' />
+      <B.Input
+        type='password'
+        defaultValue='password'
+        label='Password'
+        ref='password'
+        labelClassName='col-xs-2'
+        wrapperClassName='col-xs-4' />
+      <B.Input
+        type='submit'
+        label=' '
+        labelClassName='col-xs-2'
+        wrapperClassName='col-xs-4' />
+      </form>
     );
   }
 
