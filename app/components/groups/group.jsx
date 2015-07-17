2ce3735135523b0f87ac0950eb00c6170f37f52c
Daniel Reszka <dannn.r@gmail.com>
2015-07-17 18:35:05 +0200

    Revert "Cleanup code for Group component"
    This reverts commit f5685f24667e09a3e313488fd9eb803c02eba2ab.
    
    * Fixes bug of group main content not changing accordingly to
      group selected in the groups list menu

--- app/components/groups/group.jsx
+++ app/components/groups/group.jsx
@@ -17,34 +17,23 @@ var DiscussionsList = require('../discussions/discussions_list');
 var Discussion = require('../discussions/discussion');
 
 
-var RouteHandler = React.createClass({
-  contextTypes: { router: React.PropTypes.func },
-
-  render: function() {
-    var groupId      = this.context.router.getCurrentParams().groupId;
-    var discussionId = this.context.router.getCurrentParams().discussionId;
-
-    return (
-      <GroupLoader groupId={groupId} discussionId={discussionId} />
-    );
-  }
-});
-
-
-var GroupLoader = React.createClass({
+var Container = React.createClass({
   mixins: [Reflux.connect(GroupStore, "item")],
-
+  contextTypes: {
+    router: React.PropTypes.func
+  },
   componentWillMount: function () {
-    GroupActions.load(this.props.groupId);
+    var groupId = this.context.router.getCurrentParams().groupId;
+    GroupActions.load(groupId);
   },
-
   componentWillReceiveProps: function () {
-    GroupActions.load(this.props.groupId);
+    var groupId = this.context.router.getCurrentParams().groupId;
+    GroupActions.load(groupId);
   },
-
   render: function() {
     var group = this.state.item;
-    var discussionId = this.props.discussionId;
+    var discussionId = this.context.router.getCurrentParams().discussionId;
+
     return(
       <Group group={group} discussionId={discussionId} />
     );
@@ -106,4 +95,4 @@ var Group = React.createClass({
 });
 
 
-module.exports = RouteHandler;
+module.exports = Container;
