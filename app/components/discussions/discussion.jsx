5cf4e2d6bdbafac86e079a5c6ec9db3200b9ca81
Daniel Reszka <dannn.r@gmail.com>
2015-07-17 20:04:08 +0200

    Show discussion entries (conversations) on discussion page
    * Fetches the discussion with its entries from the backend

--- app/components/discussions/discussion.jsx
+++ app/components/discussions/discussion.jsx
@@ -3,8 +3,13 @@
 var React = require('react');
 var Reflux = require('reflux');
 
+var B = require('react-bootstrap')
+
 var DiscussionActions = require('../../actions/discussion_actions');
 var DiscussionStore   = require('../../stores/discussion_store');
+
+var PersonAvatar = require('../person_avatar');
+var Meta = require('../meta');
 
 
 var DiscussionLoader = React.createClass({
@@ -23,10 +28,63 @@ var DiscussionLoader = React.createClass({
 var Discussion = React.createClass({
   render: function() {
     var discussion = this.props.discussion;
+
     return (
       <div>
-        <h4>{discussion.title}</h4>
-        <p>{discussion.author && discussion.author.name}</p>
+        <div className="text-right">
+          <Meta follow={true} />
+        </div>
+        <EntriesList entries={discussion.entries} />
+      </div>
+    );
+  }
+});
+
+
+var Entry = React.createClass({
+  render: function() {
+    var entry = this.props.entry;
+    var author = entry.author;
+    var children_entries = entry.entries;
+
+    return(
+      <div>
+          <hr />
+          <B.Row>
+            <B.Col md={1}>
+              <PersonAvatar person={author} size={40} />
+            </B.Col>
+            <B.Col md={10}>
+              { entry.body }
+              <Meta
+                follow={this.props.follow}
+                reply={this.props.reply}
+                repost={this.props.repost} />
+              { children_entries && <EntriesList entries={children_entries} /> }
+            </B.Col>
+          </B.Row>
+      </div>
+    );
+  }
+});
+
+
+var EntriesList = React.createClass({
+  render: function() {
+    var entries = this.props.entries;
+
+    return (
+      <div>
+        { entries && entries.map(function(entry, i){
+          return(
+            <Entry
+              key={i}
+              entry={entry}
+              follow={true}
+              reply={true}
+              repost={true} />
+          );
+        })}
       </div>
     );
   }
