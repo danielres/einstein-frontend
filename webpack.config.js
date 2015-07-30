5d9637be2714c9fb2307c04d89a6f9d2721e13d5
Daniel Reszka <dannn.r@gmail.com>
2015-07-30 15:59:48 +0200

    Setup Webpack to bundle vendor code separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
+
 module.exports = {
-  entry: [
-    './app/main.jsx',
-    './node_modules/bootstrap/dist/css/bootstrap.min.css',
-    './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff',
-    './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
-    './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
-    './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2',
-    './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf'
-  ],
+  entry: {
+    app: './app/main.jsx',
+    vendors: [
+      'faker', 'lodash', 'reflux', 'react', 'react-intl',
+      'react-bootstrap', 'intl', 'react-router',
+      'react-router-bootstrap', 'superagent'
+    ],
+    bootstrap: [
+      './node_modules/bootstrap/dist/css/bootstrap.min.css',
+      './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff',
+      './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
+      './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
+      './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2',
+      './node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf'
+    ]
+  },
   resolve: {
     extensions: ['', '.js', '.jsx'],
     modulesDirectories: ['node_modules', 'app']
@@ -18,9 +29,12 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'jsx-loader' },
+      { test: /\.jsx$/, exclude: [node_modules_dir], loader: 'jsx-loader' },
       { test: /\.css$/, loader: 'file-loader?name=css/[name].[ext]' },
       { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, loader: 'file-loader?name=fonts/[name].[ext]' }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+  ]
 };
