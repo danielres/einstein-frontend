e4a2d99fa2453508da67c5cc8b99e77bcc56394c
Daniel Reszka <dannn.r@gmail.com>
2015-08-07 11:00:25 +0200

    Make login flow in fake API mimic the one from real API
    * Fixes fake API breaking test "tests/e2e/login-test.js"
    * Adds new package "body-parser"

--- api/fake-api-server.js
+++ api/fake-api-server.js
@@ -1,27 +1,33 @@
 var jsonServer = require('json-server');
 var server     = require('express')();
 var faker      = require('./faker.js');
+var bodyParser = require('body-parser');
+
+
+server.use(bodyParser.json());
 
 server.post('/login', function(req, res){
-  res
-    .header('Access-Control-Allow-Origin', '*')
-    .header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    .header('Access-Control-Allow-Headers', 'Content-Type')
-    .status(200)
-    .json({
-      id: 1,
-      email: "testuser@fake.com",
-      created_at: "2015-04-17T14:43:01.461Z",
-      updated_at: "2015-07-05T12:00:14.648Z",
-      access_token: "1:GHX7XT5oJv4KK8Do2Wiq",
-      username: "Test User",
-  })
+  if(req.body.username || req.headers.authorization){
+    res
+      .header('Access-Control-Allow-Origin', '*')
+      .header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+      .header('Access-Control-Allow-Headers', 'Content-Type')
+      .status(200)
+      .json({
+        id:           1,
+        email:        'testuser@fake.com',
+        created_at:   '2015-04-17T14:43:01.461Z',
+        updated_at:   '2015-07-05T12:00:14.648Z',
+        access_token: '1:GHX7XT5oJv4KK8Do2Wiq',
+        username:     'Test User',
+    })
+  };
 });
 
 
 server.use(jsonServer.rewriter({
-  '/groups/:groupId/discussions': '/discussions',
-  '/groups/:groupId/discussions/:id': '/discussions/:id'
+  '/groups/:groupId/discussions':     '/discussions',
+  '/groups/:groupId/discussions/:id': '/discussions/:id',
 }))
 
 server.use(jsonServer.defaults);
