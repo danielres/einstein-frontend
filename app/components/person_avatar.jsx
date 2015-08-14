c385d6d6b89047f716b0e38d3e61552aefa0794b
Daniel Reszka <dannn.r@gmail.com>
2015-08-14 14:29:43 +0200

    Extract sub-component "PersonAvatarImage" from "PersonAvatar"
    * Makes it possible to do this:
    
      - as before:
        `var PersonAvatar = require('components/person_avatar');`
    
      - new:
        `var PersonAvatarImage = require('components/person_avatar').Image;`

--- app/components/person_avatar.jsx
+++ app/components/person_avatar.jsx
@@ -3,6 +3,25 @@
 var React = require('react');
 var Link  = require('react-router').Link;
 
+var PersonAvatarImage = React.createClass({
+  displayName: 'PersonAvatarImage',
+
+  render: function() {
+    var person = this.props.person ;
+    var size   = this.props.size ;
+
+    return (
+      <img
+        alt={person.username + ' avatar'}
+        className="img-circle"
+        src={person.avatar}
+        title={person.username}
+        width={size}
+      />
+    );
+  }
+});
+
 
 var PersonAvatar = React.createClass({
   displayName: 'PersonAvatar',
@@ -16,12 +35,9 @@ var PersonAvatar = React.createClass({
         params={{personId: person.id}}
         to="person"
       >
-        <img
-          alt={person.username + ' avatar'}
-          className="img-circle"
-          src={person.avatar}
-          title={person.username}
-          width={size}
+        <PersonAvatarImage
+          person={person}
+          size={size}
         />
       </Link>
     );
@@ -30,3 +46,4 @@ var PersonAvatar = React.createClass({
 
 
 module.exports = PersonAvatar;
+module.exports.Image = PersonAvatarImage;
